15dc626c0cc2ac962af8d206c3f0e48759ddca3a
onatustun <onatustun@gmail.com>
2025-04-22 19:21:33 +0100

    rofi style

--- home/rofi.nix
+++ home/rofi.nix
@@ -19,41 +19,28 @@
 
     "rofi/themes/spotlight-dark.rasi".text = ''
       * {
-        font: "JetBrainsMono Nerd Font 12";
         background-color: transparent;
-        text-color: ${config.stylix.base16Scheme.base05}; 
+        font: "JetBrainsMono Nerd Font 12";
         margin: 0;
         padding: 0;
         spacing: 0;
+        text-color: ${config.stylix.base16Scheme.base05}; 
       }
 
       window {
         background-color: ${config.stylix.base16Scheme.base00}; 
-        location: center;
-        width: 640;
+        border-color: ${config.stylix.base16Scheme.base0D}; 
         border-radius: 8;
+        border: 2px;
+        location: center;
+        width: 540;
       }
 
       inputbar {
-        font-size: 20px; 
-        padding: 12px;
-        spacing: 12px;
         children: [ entry ];
-      }
-
-      entry, element-icon, element-text {
-        vertical-align: 0.5;
-      }
-
-      entry {
-        font: inherit;
-        placeholder-color: ${config.stylix.base16Scheme.base04}; 
-      }
-
-      message {
-        border: 2px 0 0;
-        border-color: ${config.stylix.base16Scheme.base02}; 
-        background-color: ${config.stylix.base16Scheme.base02}; 
+        font-size: 20px; 
+        padding: 16px;
+        spacing: 16px;
       }
 
       textbox {
@@ -61,40 +48,20 @@
       }
 
       listview {
-        lines: 10;
         columns: 1;
         fixed-height: false;
-        border: 1px 0 0;
-        border-color: ${config.stylix.base16Scheme.base02}; 
+        lines: 8;
       }
 
       element {
+        background-color: transparent;
         padding: 8px 16px;
         spacing: 16px;
-        background-color: transparent;
-        text-color: ${config.stylix.base16Scheme.base05}; 
-      }
-
-      element normal active {
-        text-color: ${config.stylix.base16Scheme.base0D};
-      }
-
-      element alternate active {
-        text-color: ${config.stylix.base16Scheme.base0D};
       }
 
       element selected normal,
       element selected active {
         background-color: ${config.stylix.base16Scheme.base0D}; 
-        text-color: ${config.stylix.base16Scheme.base07};       
-      }
-
-      element-icon {
-        size: 1em;
-      }
-
-      element-text {
-         text-color: inherit; 
       }
     '';
   };
