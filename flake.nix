630585e77d697f9453829ce5b2ea225f93ece3fe
onatustun <onatustun@gmail.com>
2025-04-22 02:53:29 +0100

    parts nvm nrn

--- flake.nix
+++ flake.nix
@@ -10,14 +10,38 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nvf.url = "github:notashelf/nvf";
     stylix.url = "github:danth/stylix";
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-    imports = [
-      ./user.nix
-    ];
+  outputs = inputs: let
+    system = "x86_64-linux";
+    pkgs = inputs.nixpkgs.legacyPackages.${system};
+
+    mkHost = hostPath: inputs.nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = { inherit inputs; };
+      modules = [
+        hostPath
+        { nixpkgs.config.allowUnfree = true; }
+        inputs.home-manager.nixosModules.home-manager
+        inputs.stylix.nixosModules.stylix
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = { inherit inputs system; };
+            users.onat = import ./home;
+            backupFileExtension = "homebackup";
+          };
+        }
+      ];
+    };
+  in {
+    nixosConfigurations = {
+      laptop = mkHost ./hosts/laptop;
+    };
+
+    devShells.${system}.default = import ./shell.nix { inherit pkgs; };
   };
 }
