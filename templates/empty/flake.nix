21f339fa500119178d0b10a509243a988d7895fa
onatustun <o@ust.sh>
2025-07-12 21:56:40 +0100

    nix conventions

--- templates/empty/flake.nix
+++ templates/empty/flake.nix
@@ -21,10 +21,9 @@
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       systems = import inputs.systems;
 
-      imports = let
-        inherit (inputs.nixpkgs.lib.filesystem) listFilesRecursive;
-        inherit (inputs.nixpkgs.lib) filter hasSuffix;
-      in
-        filter (file: !(builtins.elem file [./flake.nix ./shell.nix])) (filter (hasSuffix ".nix") (listFilesRecursive ./.));
+      imports = [
+        ./dev-shell.nix
+        ./pre-commit-hooks.nix
+      ];
     };
 }
