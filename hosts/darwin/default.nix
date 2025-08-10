9f203c1922ffd5db02c767c1df9802b3f0ceeb89
onatustun <o@ust.sh>
2025-08-10 02:52:36 +0100

    forgot about ignore

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -20,24 +20,17 @@ in {
       ];
 
       modules = [
-        "common/archivers"
-        "common/bat"
-        "common/cli"
-        "common/direnv"
-        "common/eza"
-        "common/fastfetch"
-        "common/fish"
-        "common/ghostty"
-        "common/git"
-        "common/helix"
-        "common/man"
-        "common/nh"
-        "common/nixpkgs"
-        "common/starship"
-        "common/stylix"
-        "common/tmux"
+        "common"
         "darwin/homebrew"
         "darwin/menu"
+      ];
+
+      ignore = [
+        "apps"
+        "discord"
+        "fonts"
+        "xdg"
+        "zen"
       ];
 
       extraModules = [
