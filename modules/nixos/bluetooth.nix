5cf21214dccef0a976daa4601adc5861a4ec8b1d
onatustun <o@ust.sh>
2025-08-11 00:59:15 +0100

    nixos.bluetooth: blueberry

--- modules/nixos/bluetooth.nix
+++ modules/nixos/bluetooth.nix
@@ -1,6 +1,11 @@
-{lib, ...}: let
+{
+  lib,
+  pkgs,
+  ...
+}: let
   inherit (lib) enabled;
 in {
   hardware.bluetooth = enabled {powerOnBoot = true;};
   services.blueman = enabled;
+  environment.systemPackages = [pkgs.blueberry];
 }
