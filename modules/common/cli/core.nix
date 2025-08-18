7310b1b9f4160247308d3bf2923f05ac4f6c395b
onatustun <o@ust.sh>
2025-08-18 15:41:10 +0100

    core tools

--- modules/common/cli/core.nix
+++ modules/common/cli/core.nix
@@ -1,9 +1,24 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
+    binutils
     bottom
+    coreutils-full
+    curl
+    diffutils
+    file
+    findutils
+    gawk
     git
+    gnugrep
+    gnused
+    jq
     killall
     less
+    lsof
+    procps
+    psmisc
     vim
+    wget
+    which
   ];
 }
