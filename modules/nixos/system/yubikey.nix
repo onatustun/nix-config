d2a51da08b70cc02649f5c22f921f51f006e4eab
onatustun <git@onatustun.com>
2025-09-02 06:08:00 +0100

    prep before pipe operators and u2fKeys organiz...

--- modules/nixos/system/yubikey.nix
+++ modules/nixos/system/yubikey.nix
@@ -5,35 +5,6 @@
 }: let
   inherit (lib) enabled;
 in {
-  environment.systemPackages = with pkgs; [
-    age-plugin-yubikey
-    fido2-manage
-    pamtester
-    pam_u2f
-    yubikey-agent
-    yubikey-manager
-    yubikey-personalization
-    yubikey-touch-detector
-    yubioath-flutter
-  ];
-
-  programs = {
-    ssh.startAgent = false;
-
-    gnupg.agent = enabled {
-      enableSSHSupport = true;
-      pinentryPackage = pkgs.pinentry-gnome3;
-    };
-  };
-
-  home-manager.sharedModules = [
-    {
-      programs.gpg = enabled {
-        scdaemonSettings.disable-ccid = true;
-      };
-    }
-  ];
-
   environment.etc."u2f-keys" = {
     text = "onat:JLXDGTk3Ga2sRBz1cCrARjYbPySed5ZGDVX+T70NBePnAAZsxeAiK0Cl4cBXUjZ+3mx/bicbocJdYBf1WpHClw==,XuyXJU85C9ytixnVMBrx5jRuEujpkQJGxW/dS9ZlhjUXzgKw1q4xivdAaN9eFP9WKmDm0RyGo/t3EtxD4wvq0w==,es256,+presence:lzcAIIfjWzUj3lXxYfCmKDMBJ3QnajAS1bjlqyNP3ece+oN75482SS5vXyPDpK1fBF0+qgzRz3BF8wuC1IUwTQ==,JzQBz79Mg9/uVf5r5J9IsrBDOgSI9hbzaEl2qWnfhSAagT+NVbpibGNRguVMtUjbfzo/jGb20xkJy1r87yelGg==,es256,+presence";
     mode = "0644";
@@ -71,4 +42,34 @@ in {
       yubikey-personalization
     ];
   };
+
+  programs = {
+    ssh.startAgent = false;
+
+    gnupg.agent = enabled {
+      enableSSHSupport = true;
+      pinentryPackage = pkgs.pinentry-gnome3;
+    };
+  };
+
+  home-manager.sharedModules = [
+    {
+      programs.gpg = enabled {
+        scdaemonSettings.disable-ccid = true;
+      };
+    }
+  ];
+
+  environment.systemPackages = with pkgs; [
+    age-plugin-yubikey
+    cryptsetup
+    fido2-manage
+    pamtester
+    pam_u2f
+    yubikey-agent
+    yubikey-manager
+    yubikey-personalization
+    yubikey-touch-detector
+    yubioath-flutter
+  ];
 }
