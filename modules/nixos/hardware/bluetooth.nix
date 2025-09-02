78c32a1058850036bc3a601201bf264f16094e7b
onatustun <git@onatustun.com>
2025-09-02 03:51:03 +0100

    printing

--- modules/nixos/hardware/bluetooth.nix
+++ modules/nixos/hardware/bluetooth.nix
@@ -5,7 +5,22 @@
 }: let
   inherit (lib) enabled;
 in {
+  services = {
+    blueman = enabled;
+
+    avahi = enabled {
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+
+    printing = enabled {
+      drivers = with pkgs; [
+        cups-filters
+        cups-browsed
+      ];
+    };
+  };
+
   hardware.bluetooth = enabled {powerOnBoot = true;};
-  services.blueman = enabled;
   environment.systemPackages = [pkgs.blueberry];
 }
