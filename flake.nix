da66a08188fe59b47cbbceb796f6118fe193fb40
onatustun <git@onatustun.com>
2025-11-30 02:13:52 +0000

    chore: accept-flake-config, cache dev, git ignore, .envrc

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
 {
+  description = "Nix config";
+
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake {inherit inputs;}
     (inputs.import-tree [
@@ -7,6 +9,30 @@
       ./modules
       ./parts
     ]);
+
+  nixConfig = {
+    extra-substituters = [
+      "https://chaotic-nyx.cachix.org"
+      "https://ghostty.cachix.org"
+      "https://helix.cachix.org"
+      "https://hyprland.cachix.org"
+      "https://niri.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://nix-darwin.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+      "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
+      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nix-darwin.cachix.org-1:LxMyKzQk7Uqkc1Pfq5uhm9GSn07xkERpy+7cpwc006A="
+    ];
+
+    extra-experimental-features = "nix-command flakes";
+  };
 
   inputs = {
     nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
