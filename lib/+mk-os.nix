40a8f4d1a247071d3190d0555fdffda138b2f3c2
onatustun <git@onatustun.com>
2026-03-17 22:25:59 +0000

    mk-os: wrap builder in `withSystem`
    `specialArgs` can inherit the `withSystem` lambda args directly
    aligns with the flake-parts example usage of `withSystem`
    https://flake.parts/module-arguments.html#withsystem

--- lib/+mk-os.nix
+++ lib/+mk-os.nix
@@ -31,70 +31,39 @@
         modules ? [],
         hmModules ? [],
         module ? {},
-      }: let
-        specialArgs =
-          inputs
-          // {
-            inherit inputs system type hostName username homeVersion hmModules;
+      }: {
+        "${type}Configurations".${hostName} = withSystem system ({
+          inputs',
+          self',
+          config,
+          ...
+        }:
+          inputs.${systems.${type}.input}.lib."${type}System" {
+            specialArgs =
+              inputs
+              // {
+                inherit (config) apps checks devShells formatter legacyPackages packages;
+                inherit inputs self inputs' self' type hostName system username homeVersion hmModules;
 
-            isDesktop = hostName == "desktop";
-            isLaptop = hostName == "laptop";
-            isServer = hostName == "server";
+                isDesktop = hostName == "desktop";
+                isLaptop = hostName == "laptop";
+                isServer = hostName == "server";
 
-            homeDir = "/${systems.${type}.homePrefix}/${username}";
-            secretsDir = self + "/secrets";
-            keys = import ./_keys.nix;
-          };
+                homeDir = "/${systems.${type}.homePrefix}/${username}";
+                secretsDir = self + "/secrets";
+                keys = import ./_keys.nix;
+              };
 
-        perSystemArgs = {
-          _module.args = withSystem system ({
-            self',
-            inputs',
-            ...
-          }: {inherit self' inputs';});
-        };
-
-        os = inputs.${systems.${type}.input}.lib."${type}System" {
-          inherit specialArgs;
-          modules = [self.modules.${type}."host-${hostName}"];
-        };
-
-        osConfig = os.config;
-      in {
-        "${type}Configurations".${hostName} = os;
+            modules = [self.modules.${type}."host-${hostName}"];
+          });
 
         modules.${type}."host-${hostName}" = {
           system = {inherit stateVersion;};
 
           imports =
-            [perSystemArgs]
-            ++ modules
+            modules
             ++ lib.lists.optional (homeVersion != null) self.modules.${type}.home-manager
             ++ [module];
-        };
-
-        homeConfigurations = lib.modules.mkIf (homeVersion != null) {
-          "${username}@${hostName}" = inputs.home-manager.lib.homeManagerConfiguration {
-            pkgs = import inputs.nixpkgs {
-              hostPlatform = {inherit system;};
-              inherit (osConfig.nixpkgs) config overlays;
-            };
-
-            extraSpecialArgs =
-              specialArgs
-              // {
-                inherit osConfig;
-                isStandalone = true;
-              };
-
-            modules =
-              [
-                perSystemArgs
-                self.modules.homeManager.home-manager
-              ]
-              ++ hmModules
-              ++ osConfig.home-manager.sharedModules;
-          };
         };
       };
   };
