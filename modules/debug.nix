f32873bb98a5ae4a2df4f0ee3beb3b711da83986
onatustun <git@onatustun.com>
2026-03-31 08:40:29 +0100

    debug: expose currentSystem in dev partition output

--- modules/debug.nix
+++ modules/debug.nix
@@ -1,8 +1,22 @@
+{ config, ... }:
 {
   partitionedAttrs = {
     allSystems = "dev";
+    currentSystem = "dev";
     debug = "dev";
   };
 
-  partitions.dev.module.debug = true;
+  partitions.dev.module =
+    { lib, ... }:
+    let
+      inherit (lib.attrsets) optionalAttrs;
+      inherit (lib.modules) mkForce;
+    in
+    {
+      debug = true;
+
+      flake = optionalAttrs (builtins ? currentSystem) {
+        currentSystem = mkForce config.flake.allSystems.${builtins.currentSystem};
+      };
+    };
 }
